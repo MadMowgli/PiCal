e43e0defdf826a3c40c7078c4d6a000840cc604a
Joel Laeubin <laeubin.j@icloud.com>
2026-04-15 12:16:03 +0200

    Cleanup, adding /events endpoint

--- auth_handler.py
+++ auth_handler.py
@@ -7,7 +7,8 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
 
-TOKEN_FILE = "token.json"
+TOKEN_FILE = r"token.json"
+CREDENTIALS_FILE = r"client_secret.json"
 SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
 
 def get_credentials():
@@ -15,17 +16,18 @@ def get_credentials():
 
     # Load cached token if it exists
     if os.path.exists(TOKEN_FILE):
-        with open(TOKEN_FILE, "r") as f:
-            credentials = Credentials.from_authorized_user_file(TOKEN_FILE, SCOPES)
+        credentials = Credentials.from_authorized_user_file(TOKEN_FILE, SCOPES)
 
+    # https://developers.google.com/workspace/calendar/api/quickstart/python
     # If no or invalid creds, auth
     if not credentials or not credentials.valid:
         if credentials and credentials.expired and credentials.refresh_token:
             credentials.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(TOKEN_FILE, SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file(CREDENTIALS_FILE, SCOPES)
             credentials = flow.run_local_server(port=0)
-        with open(TOKEN_FILE, "w") as f:
+
+        with open(TOKEN_FILE, "w+") as f:
             f.write(credentials.to_json())
 
     return credentials
--- main.py
+++ main.py
@@ -1,11 +1,77 @@
-from fastapi import FastAPI
+from datetime import datetime
+import json
+import os
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+from fastapi import FastAPI
+from googleapiclient.discovery import build
+
+import auth_handler
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    app.state.credentials = auth_handler.get_credentials()
+    app.state.service = build("calendar", "v3", credentials=app.state.credentials)
+    with open('config.json', 'r') as config_file:
+        app.state.config = json.load(config_file)
+
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+@app.get('/events')
+async def get_calendar():
+
+    # Get last 50 events from calendar
+    events = app.state.service.events().list(
+        calendarId=app.state.config['calendarId'],
+        maxResults=50
+    ).execute()
+
+    # Filter out timed events
+    timed_events = [
+        {
+            'Start': x['start']['dateTime'],
+            'End': x['end']['dateTime'],
+            'Creator': x['creator'],
+            'Summary': x['summary']
+        } for x in events['items'] if 'dateTime' in x['start']
+    ]
+
+    # Also get all day events
+    all_day_events = [
+        {
+            'Start': x['start']['date'],
+            'End': x['end']['date'],
+            'Creator': x['creator'],
+            'Summary': x['summary']
+        } for x in events['items'] if 'dateTime' not in x['start']
+
+    ]
+
+    # Merge all events, sort them by datetime
+    all_events = all_day_events + timed_events
+    all_events.sort(key=lambda x: x['Start'])
+
+    # Format the datetime of each event to make it more readable
+    for event in all_events:
+        event['Start'] = datetime.fromisoformat(event['Start']).strftime('%d.%m.%Y %H:%M:%S')
+        event['End'] = datetime.fromisoformat(event['End']).strftime('%d.%m.%Y %H:%M:%S')
+
+    return all_events
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/creds")
+async def credentials():
+    return auth_handler.get_credentials()
 
 
 @app.get("/hello/{name}")
